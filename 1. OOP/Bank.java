8e6aef90018618df91d9d492f59a8833d080c9fd
Abdus Samee <53638835+Abdus-Samee@users.noreply.github.com>
2022-05-20 20:22:25 +0600

    Update Bank.java

--- 1. OOP/Bank.java
+++ 1. OOP/Bank.java
@@ -13,15 +13,13 @@ public class Bank {
         this.employees = new Employee[8];
         this.accounts = new ArrayList<>();
 
-        this.employees[0] = new MD();
+        this.employees[0] = new MD(this, "MD");
 
         for(int i = 1; i < 3; i++){
-            this.employees[i] = new Officer();
-            this.employees[i].setBank(this);
+            this.employees[i] = new Officer(this, "S"+i);
         }
         for(int i = 3; i < 8; i++){
-            this.employees[i] = new Cashier();
-            this.employees[i].setBank(this);
+            this.employees[i] = new Cashier(this, "C"+(i-2));
         }
 
         System.out.println("Bank Created; MD, S1, S2, C1, C2, C3, C4, C5 created");
@@ -55,7 +53,7 @@ public class Bank {
     public String checkLoan(){
         int count = 0;
         for(Account account : accounts){
-            if(account.getLoanReq() > 0) count++;
+            if(account.getLoanPending() > 0) count++;
         }
 
         if(count > 0) return ", there are loan approvals pending";
@@ -74,12 +72,11 @@ public class Bank {
             System.out.printf(checkLoan());
             System.out.println();
 
-            int idx = Integer.parseInt(name.substring(0, 1))-1;
-
-            return employees[idx];
+            if(name.equals("S1")) return this.employees[1];
+            else return this.employees[2];
         }else if(name.charAt(0) == 'C'){
             System.out.printf(name + " is active");
-            int idx = Integer.parseInt(name.substring(0, 1))-1;
+            int idx = Integer.parseInt(name.substring(1))+2;
 
             return employees[idx];
         }else{
@@ -98,5 +95,13 @@ public class Bank {
     public void inc(){
         this.clock++;
         System.out.println("1 year passed");
+
+        for(Account account : this.accounts){
+            if(account.getLoanReq() > 0) {
+                account.setDeposit(account.getDeposit()+((account.getDeposit()+account.getLoanReq())*5)/100);
+                account.setDeposit(account.getDeposit() - (int) (account.getLoanReq() * account.getInterestRate()) / 100);
+                //account.setDeposit(account.getDeposit()-500);
+            }
+        }
     }
 }
