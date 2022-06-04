676811d85b9717ca5bf6be0d22cfaa7ed09a152c
Abdus Samee <53638835+Abdus-Samee@users.noreply.github.com>
2022-06-04 14:39:53 +0600

    Create Editor.java

---
+++ 2. Creational Pattern/Editor.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Editor {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        String extension = input.split(".")[1];
+    }
+}
