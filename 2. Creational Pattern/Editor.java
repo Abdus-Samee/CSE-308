bcd3aba5ee387841e77027e997c7c3dde28885b8
Abdus Samee <53638835+Abdus-Samee@users.noreply.github.com>
2022-06-04 14:40:37 +0600

    Update Editor.java

--- 2. Creational Pattern/Editor.java
+++ 2. Creational Pattern/Editor.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Editor {
     public static void main(String[] args) {
+        System.out.println("Enter file name with extension: ");
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
         String extension = input.split(".")[1];
