136a806dc5fee7697813ddf3ed631856776aab88
Abdus Samee <53638835+Abdus-Samee@users.noreply.github.com>
2022-06-17 18:58:13 +0600

    Update FileFilter.java

--- 3. Structural Pattern/Problem1/FileFilter.java
+++ 3. Structural Pattern/Problem1/FileFilter.java
@@ -1,11 +1,16 @@
 package Problem1;
 
+import java.io.File;
+
 public class FileFilter implements FileReader{
     FileAdapter fileAdapter;
 
     @Override
     public void read(String file, String delim) {
-        if(delim.equals(" ")) CalculateSum.getSumWSDelim(file);
+        if(delim.equals(" ")){
+            File input = new File(file);
+            CalculateSum.getSumWSDelim(input);
+        }
         else if(delim.equals("~")){
             fileAdapter = new FileAdapter(delim);
             fileAdapter.read(file, delim);
