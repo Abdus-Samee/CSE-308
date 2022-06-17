8c70d58ce384afca5472cc30f192babb5a614a85
Abdus Samee <53638835+Abdus-Samee@users.noreply.github.com>
2022-06-17 18:57:25 +0600

    Update AdvancedFileReader.java

--- 3. Structural Pattern/Problem1/AdvancedFileReader.java
+++ 3. Structural Pattern/Problem1/AdvancedFileReader.java
@@ -1,6 +1,6 @@
 package Problem1;
 
-import java.io.BufferedReader;
+import java.io.*;
 import java.io.FileReader;
 
 public interface AdvancedFileReader {
@@ -11,13 +11,21 @@ class DelimiterTilde implements AdvancedFileReader{
 
     @Override
     public void fileDelimTilde(String file) {
+        File temp = null;
+
         try{
+            temp = File.createTempFile("temporary", "txt");
             BufferedReader reader = new BufferedReader(new FileReader(file));
             String in = reader.readLine();
+            in = in.replaceAll("~", " ");
+
+            BufferedWriter writer = new BufferedWriter(new FileWriter(temp));
+            writer.write(in);
+            writer.close();
         }catch (Exception e){
             e.printStackTrace();
         }
 
-        CalculateSum.getSum(file, "~");
+        CalculateSum.getSumWSDelim(temp);
     }
 }
