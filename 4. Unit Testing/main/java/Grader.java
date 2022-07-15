321a3bed9d22f503da229512ec1aff15432c1df2
Abdus Samee <53638835+Abdus-Samee@users.noreply.github.com>
2022-07-15 11:46:34 +0600

    Update Grader.java

--- 4. Unit Testing/main/java/Grader.java
+++ 4. Unit Testing/main/java/Grader.java
@@ -1,14 +1,29 @@
 public class Grader {
-    public String calculateGrade(double score, int credit){
-        int marks = (int) Math.ceil(score);
-        String grade = "F";
+    public String calculateGrade(String str_score, String str_credit){
+        int credit = 0;
+        double score = 0.0;
 
-        if(marks < 0) throw new IllegalArgumentException("Marks provided is below lower bound 0.");
-        else if(marks > 400) throw new IllegalArgumentException("Marks provided is above upper bound 400.");
-        else if((credit==3 && (marks>=240 && marks<=400)) || (credit==4 && (marks>=320 && marks<=400))) grade = "A";
-        else if((credit==3 && (marks>=210 && marks<=239)) || (credit==4 && (marks>=280 && marks<=319))) grade = "B";
-        else if((credit==3 && (marks>=180 && marks<=209)) || (credit==4 && (marks>=240 && marks<=279))) grade = "C";
+        try {
+            score = Double.parseDouble(str_score);
+            credit = Integer.parseInt(str_credit);
 
-        return grade;
+            if(credit!=3 && credit!=4) throw new Exception();
+
+            int marks = (int) Math.ceil(score);
+            String grade = "F";
+
+            if (marks < 0) throw new IllegalArgumentException("Marks provided is below lower bound 0.");
+            else if (marks > 400) throw new IllegalArgumentException("Marks provided is above upper bound 400.");
+            else if ((credit == 3 && (marks >= 240 && marks <= 400)) || (credit == 4 && (marks >= 320 && marks <= 400)))
+                grade = "A";
+            else if ((credit == 3 && (marks >= 210 && marks <= 239)) || (credit == 4 && (marks >= 280 && marks <= 319)))
+                grade = "B";
+            else if ((credit == 3 && (marks >= 180 && marks <= 209)) || (credit == 4 && (marks >= 240 && marks <= 279)))
+                grade = "C";
+
+            return grade;
+        }catch(Exception e){
+            throw new NumberFormatException("Input(s) is not a number");
+        }
     }
 }
